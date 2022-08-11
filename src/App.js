286517a77e8ea6d9585a404dabb5000b07d2eaa9
Walt <jacobw56@gmail.com>
2022-08-11 15:12:43 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const fileRef = useRef();
+  const [file, setFile] = useState();
+  const [url] = useState(() => (process.env.NODE_ENV === 'development') ? process.env.REACT_APP_API_DEV_SERVER_URL : process.env.REACT_APP_API_SERVER_URL);
+
+  const showFile = async (e) => {
+    e.preventDefault();
+    console.log(e.target.files);
+    setFile(e.target.files[0]);
+  }
+
+  useEffect(() => {
+    console.log(url);
+  }, [url]);
+
+  useEffect(() => {
+    console.log(file);
+  }, [file]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('gregFile', file);
+
+    console.log(`${url}/upload`);
+
+    fetch(`${url}/upload`, {
+      method: 'post',
+      body: formData,
+    })
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          return res.json();
+        }
+        else {
+          throw new Error('Bad response!');
+        }
+      })
+      .then((result) => {
+        console.log('Success:', result);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={fileRef} type="file" onChange={(e) => showFile(e)} />
+      <br />
+      <button type="Submit" onClick={(e) => handleSubmit(e)}>Submit</button>
     </div>
   );
 }
